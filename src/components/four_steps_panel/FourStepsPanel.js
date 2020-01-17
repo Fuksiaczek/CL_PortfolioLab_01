63a56817a897e1314b91bc887385128bce1c3761
Marta Fuks <fuks.marta@wp.pl>
2020-01-17 21:42:20 +0100

    four_steps_panel new step: 1

--- src/components/four_steps_panel/FourStepsPanel.js
+++ src/components/four_steps_panel/FourStepsPanel.js
@@ -4,15 +4,33 @@ import Header from "../home/0.header/header/Header";
 import ContactForm from "../home/6.contact_form/ContactForm";
 import FourStepsPanelMainSection from "./four_steps_panel_main_section/FourStepsPanelMainSection"
 import InfoBar from "./info_bar/InfoBar";
+import ContentSectionStep1 from "./content_section/ContentSectionStep1";
 
 class FourStepsPanel extends Component
 {
+    state =
+    {
+      step1: "",
+      step2: "",
+      step3: "",
+      step4: "",
+    };
+
+    step1Update = (e, step1Value) =>
+    {
+        this.setState({
+            step1: step1Value,
+        })
+    };
+
+
     render() {
         return (
             <>
                 <Header/>
                 <FourStepsPanelMainSection/>
                 <InfoBar/>
+                <ContentSectionStep1 step1Update={(step1Value)=>this.step1Update(step1Value)}/>
                 <ContactForm/>
             </>
         )
