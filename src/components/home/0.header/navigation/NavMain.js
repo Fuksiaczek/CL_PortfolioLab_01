062518473b4638c25989e192f36be62c9151bad8
Marta Fuks <fuks.marta@wp.pl>
2020-02-26 10:48:49 +0100

    modified: header, README.md: demo

--- src/components/home/0.header/navigation/NavMain.js
+++ src/components/home/0.header/navigation/NavMain.js
@@ -11,58 +11,61 @@ class NavMain extends Component
 render() {
         return (
             <>
-                <div className="container">
-                    <nav className="nav-main nav">
-                        <ul>
-                            <li>{this.props.path === "/" ?
-                                <Link
-                                    activeClass="active"
-                                    to="scroll-main"
-                                    smooth={true}
-                                    duration={500}
-                                    delay={100}>
-                                    Start
-                                </Link>:
-                                <NavLink
-                                    to="/">
-                                    Start
-                                </NavLink>}
-                            </li>
-                            <li>
-                                <Link
-                                    activeClass="active"
-                                    to="scroll-four-steps"
-                                    smooth={true}
-                                    duration={500}
-                                    delay={100}
-                                    offset={-100}>O co chodzi?</Link>
-                            </li>
-                            <li>
-                                <Link
-                                    activeClass="active"
-                                    to="scroll-about-us"
-                                    smooth={true}
-                                    duration={500}
-                                    delay={100}>O nas</Link>
-                            </li>
-                            <li>
-                                <Link
-                                    activeClass="active"
-                                    to="scroll-who-we-help"
-                                    smooth={true}
-                                    duration={500}
-                                    delay={100}>Fundacja i organizacje</Link>
-                            </li>
-                            <li>
-                                <Link  activeClass="active"
-                                       to="scroll-contact-form"
-                                       smooth={true}
-                                       duration={500}
-                                       delay={100}>Kontakt</Link>
-                            </li>
-                        </ul>
-                    </nav>
+                <div className="sticky-nav-main">
+                    <div className="container">
+                        <nav className="nav-main nav">
+                            <ul>
+                                <li>{this.props.path === "/" ?
+                                    <Link
+                                        activeClass="active"
+                                        to="scroll-main"
+                                        smooth={true}
+                                        duration={500}
+                                        delay={100}>
+                                        Start
+                                    </Link>:
+                                    <NavLink
+                                        to="/">
+                                        Start
+                                    </NavLink>}
+                                </li>
+                                <li>
+                                    <Link
+                                        activeClass="active"
+                                        to="scroll-four-steps"
+                                        smooth={true}
+                                        duration={500}
+                                        delay={100}
+                                        offset={-100}>O co chodzi?</Link>
+                                </li>
+                                <li>
+                                    <Link
+                                        activeClass="active"
+                                        to="scroll-about-us"
+                                        smooth={true}
+                                        duration={500}
+                                        delay={100}>O nas</Link>
+                                </li>
+                                <li>
+                                    <Link
+                                        activeClass="active"
+                                        to="scroll-who-we-help"
+                                        smooth={true}
+                                        duration={500}
+                                        delay={100}>Fundacja i organizacje</Link>
+                                </li>
+                                <li>
+                                    <Link  activeClass="active"
+                                           to="scroll-contact-form"
+                                           smooth={true}
+                                           duration={500}
+                                           delay={100}>Kontakt</Link>
+                                </li>
+                            </ul>
+                        </nav>
+                    </div>
                 </div>
+
             </>
         )
     }
